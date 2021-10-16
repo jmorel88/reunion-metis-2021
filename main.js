053cccf77d59606ba6b75361d5b8143be989df89
Jose Morel <jmorel88@yahoo.com>
2021-10-16 12:28:34 +0400

    prep for demo

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const testing = false;
 
 const stats = new Stats();
 stats.showPanel(0);
-document.body.appendChild(stats.dom);
+//document.body.appendChild(stats.dom);
 
 const mapRange = (value, inMin, inMax, outMin, outMax) => {
   return ((value - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
